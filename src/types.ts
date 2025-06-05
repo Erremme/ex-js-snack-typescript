4a04fa223508882b793654bc35ac21f040bd660a
Erremme <Erremme2.0@gmail.com>
2025-06-05 17:47:02 +0200

    Create intersections

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,31 @@
- export type Dipendente = {nome : "string", 
-    cognome : "string" , 
+export type Developer = Dipendente &{
+    livelloDiEsperienza : "Junior"| "Mid" | "Senior",
+    linguaggi? : string[],
+    certificazioni : string[] 
+
+}
+
+export type ProjectManager = Dipendente &{
+    teamSize : number | null,
+    budgetGestito? : number,
+    stakeholderPrincipali : string[] 
+}
+
+
+export type Dipendente = {
+    nome : string, 
+    cognome : string , 
     annoNascita : number ,
     sesso : "m" | "f" ,
     anniDiServizio : number[]
-    readonly emmailAziendale :"string",
+    readonly emailAziendale :string,
     contratto : "indeterminato" |"determinato" | "freelance" 
+    ruolo : Developer| ProjectManager
+}
+
+export type Team = {
+    nome : string
+    progettoAttuale : string | null,
+    budget : number ,
+    membri : [ProjectManager , Developer , ...Developer[]]
 }
